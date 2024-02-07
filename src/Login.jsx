f170ad95b6684bcb9bcabad692b6cef4c4a609c9
Sullivan1301 <jorosullivan13@gmail.com>
2024-02-07 17:16:11 +0300

    feat: create all files and configurationbefore coding

---
+++ src/Login.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import './App.css'
+import { React } from "react";
+const Login = () => {
+  return (
+    <>
+    <div className='h-screen flex'>
+        <div className='hidden lg:flex'></div>
+    </div>
+    </>
+  )
+}
+
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-import { ThemeProvider } from "@material-tailwind/react";
+import { BrowserRouter } from 'react-router-dom';
 
-
+// eslint-disable-next-line react/no-deprecated
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <ThemeProvider>
-     <App />
-     </ThemeProvider>
-   
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
