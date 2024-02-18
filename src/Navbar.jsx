e4a4bc393dd5f169748dd2937244f9ce2c6a2ade
Sullivan1301 <jorosullivan13@gmail.com>
2024-02-18 22:49:44 +0300

    fix: change style

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,14 +5,18 @@ import { FaFileUpload, FaFolder, FaSignOutAlt } from 'react-icons/fa';
 
 const Navbar = () => {
     return (
-        <nav className="bg-blue-500 p-4 flex justify-between items-center">
-            <div className="logo text-white text-lg">
-                <h1>CielUX</h1>
-            </div>
+        <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-blue-600 mb-3">
+            <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="#">
+              CielUX
+            </a>
             <div className="nav-links">
-                <Link to="/add-file" className="text-white mr-4"><FaFileUpload /> Add File</Link>
-                <Link to="/add-folder" className="text-white mr-4"><FaFolder /> Add Folder</Link>
-                <Link to="/logout" className="text-white"><FaSignOutAlt /> Logout</Link>
+                <Link to="/add-file" className="text-white hover:text-white mr-4"><FaFileUpload /> Add File</Link>
+                <Link to="/add-folder" className="text-white hover:text-white mr-4"><FaFolder /> Add Folder</Link>
+                <Link to="/logout" className="text-white hover:text-white mr-4 "><FaSignOutAlt /> Logout</Link>
+            </div>
+            </div>
             </div>
         </nav>
     );
