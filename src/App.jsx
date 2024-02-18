a63253fb020c0a119bc3319c33e62c2f3a092166
Sullivan Joro <119728329+Sullivan1301@users.noreply.github.com>
2024-02-18 22:52:16 +0300

    Last version before deployement
    * fix: preprod have bugs so i push to preprod before create new preprod
    
    * chore: conflits
    
    * feat: add more informations for file uploaded
    
    * fix: add loading state
    
    * fix: add error on folder naming
    
    * fix: add error on login when email and password is not require
    
    * style add icons
    
    * indentation
    
    * fix: use Link to Logout so rewrite the logic of deconnexion
    
    * fix: add dependencies @vitejs/plugin-react
    
    * fix: add dependencies @vitejs/plugin-react
    
    * fix: Switch has removing because not available for last version of react
    
    * fix: add route in routes
    
    * fix: change style

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-/* eslint-disable no-unused-vars */
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import Login from './Login';
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Home from './Home';
+import AddFile from './AddFile';
+import AddFolder from './AddFolder';
+import Logout from './Logout';
+import Login from './Login';
 import Register from './Register';
 
 function App() {
   return (
-    <div className='flex h-screen'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Navigate to="/login" />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          {/* Ajoutez d'autres routes ici */}
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Router>
+      <Routes>
+        {/* Rediriger de la route racine vers /login */}
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        {/* Définir les routes pour les différents composants */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/add-file" element={<AddFile />} />
+        <Route path="/add-folder" element={<AddFolder />} />
+        <Route path="/logout" element={<Logout />} />
+      </Routes>
+    </Router>
   );
 }
 
